a5a24a49e71e1defd945a4cec06ae417232a2312
Alfino <nikitamalashkin73@gmail.com>
2025-05-30 13:41:35 +0500

    fixed main section render

--- src/widgets/MainPage/MainSection/ui/MainSection.tsx
+++ src/widgets/MainPage/MainSection/ui/MainSection.tsx
@@ -1,12 +1,18 @@
 'use client'
 
+import Image from 'next/image'
 import { useTranslations } from 'next-intl'
+
 import { Header } from '@/widgets/Header'
+import MainBgDesk from '@/app/assets/images/main-bg-desk.webp'
+import MainBgMobile from '@/app/assets/images/main-bg-mobile.webp'
 
 import s from './MainSection.module.scss'
+import { useMediaQuery } from '@/shared/lib'
 
 export const MainSection = () => {
 	const t = useTranslations('lending.mainSection')
+	const isMobile = useMediaQuery('(max-width: 768px)')
 
 	return (
 		<section className={s.wrapper}>
@@ -14,11 +20,29 @@ export const MainSection = () => {
 				<div className="container-middle">
 					<Header variant="lending-variant" />
 					<div className={s.inner}>
-						<h1
-							className={s.title}
-							dangerouslySetInnerHTML={{ __html: t('title') }}
-						/>
-						<p className="lending-descr">{t('description')}</p>
+						<div className={s.topContent}>
+							<h1
+								className={s.title}
+								dangerouslySetInnerHTML={{ __html: t('title') }}
+							/>
+							<p className="lending-descr">{t('description')}</p>
+						</div>
+
+						<div className={s.bg}>
+							{isMobile ? (
+								<Image
+									src={MainBgMobile}
+									alt="изображение телефонов"
+									priority
+								/>
+							) : (
+								<Image
+									src={MainBgDesk}
+									alt="изображение телефонов"
+									priority
+								/>
+							)}
+						</div>
 					</div>
 				</div>
 			</div>
