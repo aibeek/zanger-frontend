520181d90325959467d46fc7cd5f82f08df211e9
Alfino <nikitamalashkin73@gmail.com>
2025-07-07 13:10:04 +0500

    fix subscription bug 2

--- src/entities/profile/ui/ProfileSubscription/ProfileSubscription.tsx
+++ src/entities/profile/ui/ProfileSubscription/ProfileSubscription.tsx
@@ -5,15 +5,14 @@ import { useTranslations } from 'next-intl'
 export const ProfileSubscription = () => {
 	const t = useTranslations('profile.subscription')
 	const personalData = useLoginStore((state) => state.personalData)
-	const subscription =
-		personalData.lawyer.subscription !== null ? personalData.lawyer.subscription.plan.name : 'Отсутствует'
-	const end_at = personalData.lawyer.subscription.ends_at ?? ''
+	const subscriptionPlan = personalData.lawyer.subscription?.plan?.name ?? 'Отсутствует'
+	const end_at = personalData.lawyer.subscription?.ends_at ?? ''
 
 	return (
 		<div className={s.item}>
 			<h6 className={s.title}>{t('title')}</h6>
 			<span className={s.plan}>
-				{subscription} - Активна до {end_at}
+				{subscriptionPlan} - Активна до {end_at}
 			</span>
 		</div>
 	)
