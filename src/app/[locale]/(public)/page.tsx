09c55437bad71c8d178a8cc66cdba6132a5956ac
aibeek <aibekfestival23@gmail.com>
2025-10-16 17:07:08 +0500

    feat: Add Scroll to Top button with visibility toggle and styling

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,4 +1,4 @@
-import { DescriptionSection, MainSection, TeamSection, AboutSection, ModulesSection, ResourcesSection, NewsSection } from '@/widgets/MainPage'
+import { DescriptionSection, MainSection, TeamSection, AboutSection, ModulesSection, ResourcesSection, NewsSection, ScrollTopButton } from '@/widgets/MainPage'
 import { PulseChatWidget } from '@/widgets/PulseChatWidget'
 import { Footer } from '@/widgets/Footer'
 import s from './page.module.scss'
@@ -6,6 +6,7 @@ export default function Home() {
 	return (
 		<div className={s.page}>
 				<MainSection />
+				<ScrollTopButton />
 				<AboutSection />
 				<TeamSection />
 				<DescriptionSection />
